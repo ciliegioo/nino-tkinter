a5dd973c5e748683fe1e254a28f137b79b2ff62c
조은경 <129392606+ciliegioo@users.noreply.github.com>
2023-10-28 09:24:32 +0900

    Update slot3.py

--- slot3.py
+++ slot3.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageTk
 
 window = tk.Tk()
 
-window.title("Slot Machine Game")
+window.title("Slot Machine")
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 window.geometry(f"{screen_width}x{screen_height}")
@@ -17,6 +17,10 @@ canvas = tk.Canvas(window, width=screen_width,
 canvas.pack()
 
 
+# @@@@@@@@@
+# 이미지
+# @@@@@@@@@
+# 안내박스
 cherry_s = ImageTk.PhotoImage(Image.open("imge/cherry_s.png"))
 cherry_s_label = tk.Label(window, image=cherry_s, relief="ridge")
 cherry_s_label1 = tk.Label(window, image=cherry_s, relief="ridge")
@@ -35,17 +39,39 @@ seveno_s_label1 = tk.Label(window, image=seveno_s, relief="ridge")
 seveno_s_label2 = tk.Label(window, image=seveno_s, relief="ridge")
 seveno_s_label3 = tk.Label(window, image=seveno_s, relief="ridge")
 seveno_s_label4 = tk.Label(window, image=seveno_s, relief="ridge")
-
 logo = ImageTk.PhotoImage(Image.open("imge/logo4.png"))
 logo_label = tk.Label(window, image=logo, background='cadetblue3')
 one = ImageTk.PhotoImage(Image.open("imge/one.png"))
 one_label = tk.Label(window, image=one, background='cadetblue3')
-one_label2 = tk.Label(window, image=one, background='cadetblue3')
 two = ImageTk.PhotoImage(Image.open("imge/two.png"))
 two_label = tk.Label(window, image=two, background='cadetblue3')
-two_label2 = tk.Label(window, image=two, background='cadetblue3')
 three = ImageTk.PhotoImage(Image.open("imge/three.png"))
 three_label = tk.Label(window, image=three, background='cadetblue3')
+# 별
+star = ImageTk.PhotoImage(Image.open("imge/star.png"))
+star_label = tk.Label(window, image=star, background='white')
+star_label2 = tk.Label(window, image=star, background='white')
+star_label3 = tk.Label(window, image=star, background='white')
+star_label4 = tk.Label(window, image=star, background='white')
+star_label5 = tk.Label(window, image=star, background='white')
+star_label6 = tk.Label(window, image=star, background='white')
+star_label7 = tk.Label(window, image=star, background='white')
+star_label8 = tk.Label(window, image=star, background='white')
+star_label9 = tk.Label(window, image=star, background='white')
+# 심볼
+symbol_images = [
+    "imge/symbol/seveno.png",
+    "imge/symbol/cherry.png",
+    "imge/symbol/lemon.png",
+]
+symbol_images_tk = [ImageTk.PhotoImage(
+    Image.open(image)) for image in symbol_images]
+label1 = tk.Label(window, image=random.choice(
+    symbol_images_tk), borderwidth=10, relief="sunken")
+label2 = tk.Label(window, image=random.choice(
+    symbol_images_tk), borderwidth=10, relief="sunken")
+label3 = tk.Label(window, image=random.choice(
+    symbol_images_tk), borderwidth=10, relief="sunken")
 
 
 # @@@@@@@@@
@@ -55,32 +81,28 @@ canvas.create_rectangle(4, 20, screen_width-10, 130,
                         outline='gray21', width=12)
 canvas.create_rectangle(14, 30, screen_width-20, 123,
                         fill='cadetblue3', outline='indianred2', width=8)
-cherry_s_label.place(x=22, y=35)
-cherry_s_label1.place(x=102, y=35)
-cherry_s_label2.place(x=182, y=35)
-lemon_s_label.place(x=336, y=35)
-lemon_s_label1.place(x=416, y=35)
-lemon_s_label2.place(x=496, y=35)
-seveno_s_label.place(x=650, y=35)
-seveno_s_label1.place(x=730, y=35)
-seveno_s_label2.place(x=810, y=35)
-cherry_s_label3.place(x=964, y=35)
-cherry_s_label4.place(x=1044, y=35)
-seveno_s_label3.place(x=1124, y=35)
-lemon_s_label3.place(x=screen_width-275, y=35)
-lemon_s_label4.place(x=screen_width-195, y=35)
-seveno_s_label4.place(x=screen_width-111, y=35)
-
-
-# @@@@@@@@@
-# 로고
-# @@@@@@@@@
 logo_label.place(x=screen_width/2, y=2, anchor='n')
-one_label.place(x=142, y=118, anchor='n')
-one_label2.place(x=456, y=118, anchor='n')
-two_label.place(x=1084, y=118, anchor='n')
-two_label2.place(x=screen_width-155, y=118, anchor='n')
-three_label.place(x=screen_width/2, y=118, anchor='n')
+# 1
+one_label.place(x=22, y=75, anchor='w')
+cherry_s_label.place(x=62, y=35)
+cherry_s_label1.place(x=142, y=35)
+cherry_s_label2.place(x=222, y=35)
+lemon_s_label.place(x=316, y=35)
+lemon_s_label1.place(x=396, y=35)
+lemon_s_label2.place(x=476, y=35)
+# 2
+two_label.place(x=610, y=75, anchor='w')
+cherry_s_label3.place(x=660, y=35)
+cherry_s_label4.place(x=740, y=35)
+seveno_s_label3.place(x=820, y=35)
+lemon_s_label3.place(x=914, y=35)
+lemon_s_label4.place(x=994, y=35)
+seveno_s_label4.place(x=1074, y=35)
+# 3
+three_label.place(x=1212, y=75, anchor='w')
+seveno_s_label.place(x=screen_width-275, y=35)
+seveno_s_label1.place(x=screen_width-195, y=35)
+seveno_s_label2.place(x=screen_width-111, y=35)
 
 
 # @@@@@@@@@
@@ -89,14 +111,23 @@ three_label.place(x=screen_width/2, y=118, anchor='n')
 canvas.create_rectangle(386, 166, 1134, 660, outline='gray21', width=15)
 canvas.create_rectangle(396, 176, 1124, 650,
                         fill='white', outline='indianred2', width=15)
+star_label.place(x=410, y=187)
+star_label2.place(x=screen_width/2, y=187, anchor='n')
+star_label3.place(x=1075, y=187)
+star_label4.place(x=screen_width/4+205, y=187, anchor='n')
+star_label5.place(x=935, y=187, anchor='n')
+star_label6.place(x=410, y=597)
+star_label7.place(x=1075, y=597)
+star_label8.place(x=screen_width/4+205, y=597, anchor='n')
+star_label9.place(x=935, y=597, anchor='n')
 
 # @@@@@@@@@
 # 포인트
 # @@@@@@@@@
 points = 0
 points_label = tk.Label(window, text=f"Points: {points}", font=(
-    "Times New Roman", 20), background='cadetblue3')
-points_label.place(x=760, y=600, anchor='n')
+    "Times New Roman", 25), background='cadetblue3')
+points_label.place(x=760, y=596, anchor='n')
 
 
 def p_reset():
@@ -117,7 +148,6 @@ button.place(x=760, y=700, anchor='n')
 canvas.create_rectangle(236, 236, 1284, 584, outline='gray21', width=15)
 canvas.create_rectangle(246, 246, 1274, 574,
                         fill='cadetblue1', outline='indianred2', width=15)
-
 canvas.create_rectangle(296, 296, 524, 524, outline='gray21', width=30)
 canvas.create_rectangle(646, 296, 874, 524, outline='gray21', width=30)
 canvas.create_rectangle(996, 296, 1224, 524, outline='gray21', width=30)
@@ -125,26 +155,14 @@ canvas.create_rectangle(296, 296, 524, 524, outline='indianred2', width=15)
 canvas.create_rectangle(646, 296, 874, 524, outline='indianred2', width=15)
 canvas.create_rectangle(996, 296, 1224, 524, outline='indianred2', width=15)
 
-symbol_images = [
-    "imge/symbol/seveno.png",
-    "imge/symbol/cherry.png",
-    "imge/symbol/lemon.png",
-]
-
-symbol_images_tk = [ImageTk.PhotoImage(
-    Image.open(image)) for image in symbol_images]
-
-label1 = tk.Label(window, image=random.choice(
-    symbol_images_tk), borderwidth=10, relief="sunken")
-label2 = tk.Label(window, image=random.choice(
-    symbol_images_tk), borderwidth=10, relief="sunken")
-label3 = tk.Label(window, image=random.choice(
-    symbol_images_tk), borderwidth=10, relief="sunken")
-
 label1.place(x=300, y=300)
 label2.place(x=650, y=300)
 label3.place(x=1000, y=300)
 
+
+# @@@@@@@@@
+# 슬롯 설정
+# @@@@@@@@@
 rolling_symbols = [tk.Label(window, image=random.choice(
     symbol_images_tk), borderwidth=10, relief="sunken") for _ in range(3)]
 
@@ -195,7 +213,7 @@ def spin_machine():
 
 
 # @@@@@@@@@
-# 버튼
+# 실행
 # @@@@@@@@@
 start_button = tk.Button(window, text='Start', command=start_spin,
                          bg='indianred2', font=("Times New Roman", 20))
@@ -204,5 +222,6 @@ start_button.place(x=650, y=700, anchor='n')
 stop_button = tk.Button(window, text='Stop', command=stop_spin,
                         bg='indianred2', font=("Times New Roman", 20))
 stop_button.place(x=860, y=700, anchor='n')
+
 
 window.mainloop()
